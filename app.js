1fcd7bc86b3b185b0bab934323c057adc60e809c
Shaan Jain <shaan@Absolute-Sigma.local>
2023-09-21 19:03:55 +0530

    trying to add some more programs

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-const EventEmitter = require('events')  // declaration of a class
+// const server = http.createServer((req,res)=>{
+//     res.write('Welcome to my homepage');
+//     res.end();
+// })
 
-const customEmitter = new EventEmitter();
 
-customEmitter.on('response',()=>{
-    console.log(`Data received`)
-})
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.end("Welcome to our homepage");
+//     } else if (req.url === '/about') {
+//         res.end('This is about us');
+//     } else {
+//         res.end(`
+//         <h1> Error 404 </h1>
+//         <p>Can't seem to find the page you are looking for...</p>
+//         <a href="/">Back To Home Page</a>
+//         `);
+//     }
+// });
 
-customEmitter.emit('response') 
-// If I place this emit() above the on() then nothing will come on the console as it only performs
-// those tasks that are above the emit()
+const http = require('http');
 
-customEmitter.on('greet',(name, id)=>{
-    console.log(`Hello user ${name} with id : ${id}`)
-})
+// Create an HTTP server
+const server = http.createServer();
 
-customEmitter.emit('greet','Oxallion',34);
+// Register an event listener for the 'request' event
+server.on('request', (req, res) => {
+  // Emit a custom 'welcome' event
+  server.emit('welcome', req, res);
+});
+
+// Register an event listener for the custom 'welcome' event
+server.on('welcome', (req, res) => {
+  res.end('Welcome to the server!');
+});
+
+// Start the server and listen on port 5000
+server.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
+
